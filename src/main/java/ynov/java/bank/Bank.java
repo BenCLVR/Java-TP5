a3175543ce92980b935544f11454893eab20fbd5
Joffrey <joffrey.jeunehomme@ynov.com>
2022-03-18 16:48:42 +0100

    Bank.java : ajout des fonctions Jackson pour load et save en json les BankAccounts et les Users

--- src/main/java/ynov/java/bank/Bank.java
+++ src/main/java/ynov/java/bank/Bank.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -14,10 +15,24 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 
-public class Bank {
+public class Bank
+{
+	// VARIABLES POOL
+	private ArrayList<BankAccount> bankAccountPool = new ArrayList<BankAccount>();
+	private ArrayList<User> userPool = new ArrayList<User>();
+	
+	// JACKSON my boy
+	private ObjectMapper objectMapper = new ObjectMapper();
+	private String dir = System.getProperty("user.dir");
+	private String bankAccountsJsonFilePath = "\\ressources\\bankAccounts.json";
+	private String usersJsonFilePath = "\\ressources\\users.json";
 
 	public Bank() {
 		JFrame frame = new JFrame("Exemple");
@@ -68,4 +83,81 @@ public class Bank {
 
 		});
 	}
+	
+	private ArrayList<BankAccount> loadBankAccounts()
+	{
+		ArrayList<BankAccount> bankAccounts = null;
+
+		try
+		{
+			bankAccounts = objectMapper.readValue(new File(this.dir + this.bankAccountsJsonFilePath), new TypeReference<ArrayList<BankAccount>>(){});
+		}
+		catch (JsonParseException e) {
+			e.printStackTrace();
+			bankAccounts = new ArrayList<BankAccount>();
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+			bankAccounts = new ArrayList<BankAccount>();
+		} catch (IOException e) {
+			e.printStackTrace();
+			bankAccounts = new ArrayList<BankAccount>();
+		}
+		
+		return (bankAccounts);
+	}
+	private int saveBankAccounts()
+	{
+		try {
+			objectMapper.writeValue(new File(this.dir + this.bankAccountsJsonFilePath), this.bankAccountPool);
+		} catch (JsonGenerationException e) {
+			e.printStackTrace();
+			return (1);
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+			return (2);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return (3);
+		}
+		return (0);
+	}
+	
+	private ArrayList<User> loadUsers()
+	{
+		ArrayList<User> users = null;
+
+		try
+		{
+			users = objectMapper.readValue(new File(this.dir + this.usersJsonFilePath), new TypeReference<ArrayList<User>>(){});
+		}
+		catch (JsonParseException e) {
+			e.printStackTrace();
+			users = new ArrayList<User>();
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+			users = new ArrayList<User>();
+		} catch (IOException e) {
+			e.printStackTrace();
+			users = new ArrayList<User>();
+		}
+		
+		return (users);
+	}
+	private int saveUsers()
+	{
+		try {
+			objectMapper.writeValue(new File(this.dir + usersJsonFilePath), this.userPool);
+		} catch (JsonGenerationException e) {
+			e.printStackTrace();
+			return (1);
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+			return (2);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return (3);
+		}
+		return (0);
+	}
+
 }
