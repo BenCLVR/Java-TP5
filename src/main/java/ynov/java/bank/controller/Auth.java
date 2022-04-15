76ccda2dc0e8ab41f100e44f867c95ae39244ebc
Claverie Benjamin <benjamin.claverie@ynov.com>
2022-04-15 16:12:31 +0200

    map bankAccount with user

--- src/main/java/ynov/java/bank/controller/Auth.java
+++ src/main/java/ynov/java/bank/controller/Auth.java
@@ -6,9 +6,11 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import ynov.java.bank.modele.BankAccount;
 import ynov.java.bank.modele.User;
 
 public class Auth {
@@ -16,58 +18,64 @@ public class Auth {
 
 	Connexion conn = new Connexion();
 	User currentUser = null;
-	
-	public int createUser(String name, String surname, String pwd ) throws EOFException, SQLException {
+
+	public int createUser(String name, String surname, String pwd) throws EOFException, SQLException {
 		Connection sql = conn.getConnexion();
 		Statement state = sql.createStatement();
-		int result = state.executeUpdate("INSERT INTO users (nom, prenom, mdp) VALUES ('"+name+"','"+surname+"','"+pwd+"')");
+		int result = state.executeUpdate(
+				"INSERT INTO users (nom, prenom, mdp) VALUES ('" + name + "','" + surname + "','" + pwd + "')");
 		sql.close();
 		System.out.println("User added");
 		return result;
 	}
-	
-	public boolean LogUser(String name, String pwd ) throws EOFException, SQLException {
+
+	public boolean LogUser(String name, String pwd) throws EOFException, SQLException {
 		String Rpwd = new String();
 		Connection sql = conn.getConnexion();
 		Statement state = sql.createStatement();
-		
+
 		int tempId = 0;
 		String tempNom = null;
 		String tempPrenom = null;
 		String tempMdp = null;
 
-		ResultSet result = state.executeQuery("SELECT id, nom, prenom, mdp FROM users WHERE nom = '"+name+"'");
-
+		ResultSet result = state.executeQuery("SELECT id, nom, prenom, mdp FROM users WHERE nom = '" + name + "'");
 		while (result.next()) {
 			tempId = result.getInt("id");
 			tempNom = result.getString("nom");
 			tempPrenom = result.getString("prenom");
 			tempMdp = result.getString("mdp");
-			System.out.println(tempNom +" " +tempPrenom +" " +tempMdp);
+			System.out.println(tempNom + " " + tempPrenom + " " + tempMdp);
 			System.out.println("");
 			Rpwd = tempMdp;
 		}
+		state.close();
 
-		// si on a un resultat de la db (un user trouvé) et que les mdp correspondent on valide
-		if(Rpwd != null && pwd.equals(Rpwd)){
-			System.out.println("loggin succeced");
-			this.currentUser = new User(tempId, tempNom, tempPrenom, tempMdp);
-			System.out.println(currentUser.pseudo +" " +currentUser.password);
-			return true;
+		// si on a un resultat de la db (un user trouvé) et que les mdp correspondent on
+		// valide
+		if (Rpwd.equals(null) || !pwd.equals(Rpwd)) {
+			System.out.println("loggin failed");
+			return false;
 		}
 
-		result = state.executeQuery("SELECT id_account from linkaccount where id_user = '"+this.currentUser.id+"'");
-		List<Integer> accountIds = new ArrayList<Integer>();
-		while (result.next()) {
-			accountIds.add(result.getInt("id_account"));
+		System.out.println("loggin succeced");
+		this.currentUser = new User(tempId, tempNom, tempPrenom, tempMdp);
+		System.out.println(currentUser.pseudo + " " + currentUser.password);
+
+		List<Integer> BAIds = BankAccountController.getBankAccountIdsByUser(tempId);
+		System.out.println("BAIds ");
+		System.out.println(BAIds.size());
+		if(this.currentUser.bankAccounts == null) {
+			this.currentUser.setBankAccounts(new ArrayList<BankAccount>());
 		}
 
-		// String concatenedIds = accountIds.stream().map(String::valueOf).collect(Collectors.joining(","));
-		System.out.println(accountIds.toString());
+		for (Integer id : BAIds) {
+			this.currentUser.bankAccounts.add(BankAccountController.getBankAccountById(id));
+		}
 
+		System.out.println(this.currentUser.bankAccounts.size());
 
-		System.out.println("loggin failed");
-		return false;
+		return true;
 	}
 
 }
--- src/main/java/ynov/java/bank/modele/User.java
+++ src/main/java/ynov/java/bank/modele/User.java
@@ -1,8 +1,10 @@
 package ynov.java.bank.modele;
 
+import java.util.List;
+
 public class User {
 	public int id;
-	public BankAccount[] bankAccounts;
+	public List<BankAccount> bankAccounts;
 	public String nom;
 	public String pseudo;
 	public String password;
@@ -23,11 +25,11 @@ public class User {
 		this.id = id;
 	}
 
-	public BankAccount[] getBankAccounts() {
+	public List<BankAccount> getBankAccounts() {
 		return bankAccounts;
 	}
 
-	public void setBankAccounts(BankAccount[] bankAccounts) {
+	public void setBankAccounts(List<BankAccount> bankAccounts) {
 		this.bankAccounts = bankAccounts;
 	}
 
